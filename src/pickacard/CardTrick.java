4f446a4c452b299c6f3558c064104f3aa60aaa1b
Aswin Joy <joyas@sheridancollege.ca>
2021-06-03 21:09:25 -0400

    import Random and scanner

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,4 +1,7 @@
 package pickacard;
+import java.util.Scanner;
+import java.util.Random;
+
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
@@ -14,10 +17,28 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
+        Scanner game = new Scanner(System.in);
+            Random cards = new Random();
+           
+            System.out.println("The cards in the magic hand are: ");
+            
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
+            c.setValue(cards.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[cards.nextInt(4)]); 
+            magicHand[i] = c;
+            System.out.println(c);
+            
+        }
            
+        System.out.print("Enter card value: ");
+        int val = game.nextInt();
+        System.out.print("Enter suit: ");
+        String suit = game.next();
+        Card key = new Card();
+        key.setValue(val);
+        key.setSuit(suit);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
@@ -27,4 +48,4 @@ public class CardTrick {
         //Then report the result here
     }
 
-}
+
